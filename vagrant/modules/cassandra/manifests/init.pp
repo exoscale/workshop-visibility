4fdbcec7078cf89d2724a96819f112484c8242c8
Bruno Renié <brutasse@gmail.com>
2014-05-17 15:22:55 +0200

    Do not depend on pgp protocol

--- vagrant/modules/cassandra/manifests/init.pp
+++ vagrant/modules/cassandra/manifests/init.pp
@@ -1,19 +1,7 @@
 class cassandra {
 
   file { '/etc/apt/sources.list.d/cassandra.list':
-    content => "deb http://www.apache.org/dist/cassandra/debian 20x main\n"
-  }
-
-  exec { 'gpg --keyserver pgp.mit.edu --recv-keys 2B5C1B00':
-    path => "/bin:/usr/bin",
-    unless => "gpg --list-keys | grep 2B5C1B00"
-  }
-
-  exec { 'gpg --export --armor 2B5C1B00 | sudo apt-key add -':
-    path => "/bin:/usr/bin",
-    unless => "apt-key list | grep 2B5C1B00",
-    require => Exec['gpg --keyserver pgp.mit.edu --recv-keys 2B5C1B00'],
-    notify => Exec['apt-get update']
+    ensure => absent
   }
 
   exec { 'apt-get update':
@@ -22,8 +10,7 @@ class cassandra {
   }
 
   package { 'cassandra':
-    ensure => installed,
-    require => Exec['gpg --export --armor 2B5C1B00 | sudo apt-key add -']
+    ensure => installed
   }
 
   service { 'cassandra':
